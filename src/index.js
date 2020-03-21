e5aa593b3606dc477323a96d622dfe20dcdfd97b
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2020-03-21 01:38:07 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import Dashboard from './components/Dashboard/Dashboard';
 import * as serviceWorker from './serviceWorker';
+import {createMuiTheme} from '@material-ui/core/styles';
+import {ThemeProvider} from '@material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#fff',
+        },
+    },
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={darkTheme}>
+        <Dashboard/>
+    </ThemeProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
