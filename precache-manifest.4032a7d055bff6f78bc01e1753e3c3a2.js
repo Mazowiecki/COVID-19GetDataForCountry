efcb2753c16dd432054cd189a74f112c22a73fba
Stanislaw Mazowiecki <stmazowiecki@gmail.com>
2020-03-21 02:01:17 +0100

    Updates

---
+++ precache-manifest.4032a7d055bff6f78bc01e1753e3c3a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd69a73c7374b1b7503dd0b312154ee",
+    "url": "/COVID-19GetDataForCountry/index.html"
+  },
+  {
+    "revision": "ebd69d2a1f9ba1444ef8",
+    "url": "/COVID-19GetDataForCountry/static/css/main.d02e64b3.chunk.css"
+  },
+  {
+    "revision": "b02a4ab9134f9ad90563",
+    "url": "/COVID-19GetDataForCountry/static/js/2.506033f2.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/COVID-19GetDataForCountry/static/js/2.506033f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd69d2a1f9ba1444ef8",
+    "url": "/COVID-19GetDataForCountry/static/js/main.2e1c7a25.chunk.js"
+  },
+  {
+    "revision": "cbca1b6a473a1e2ec273",
+    "url": "/COVID-19GetDataForCountry/static/js/runtime-main.6cdc7e07.js"
+  }
+]);
